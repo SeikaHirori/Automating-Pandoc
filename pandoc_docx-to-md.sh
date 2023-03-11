a68eebd12c7791f9ff1ae20a958f0018130142c4
SeikaHirori <SeikaHirori@outlook.com>
2023-03-11 13:31:26 -0800

    created script for converting docx to Markdown

--- pandoc_docx-to-md.sh
+++ pandoc_docx-to-md.sh
@@ -7,4 +7,20 @@ hello_world="Hello, fellow lovely people :3"
 input_format=".docx"
 output_format=".md"
 
-echo $hello_world
+subdirectory="_conversion_Markdown"
+
+finished_conversion="Finished converting. Enjoy your day :3"
+
+mkdir_conversion() {
+    mkdir -p $subdirectory
+}
+
+convert_command() {
+    echo "Running conversion"
+    find . -name \*.docx -type f -exec pandoc -o $subdirectory/{}.md {} \;
+}
+
+mkdir_conversion
+convert_command
+
+echo $finished_conversion
