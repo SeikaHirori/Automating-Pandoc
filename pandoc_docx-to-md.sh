9548f06414ff2a579579956a2dda16f89d5306eb
SeikaHirori <SeikaHirori@outlook.com>
2023-03-11 13:02:01 -0800

    experimenting with writing scripts

---
+++ pandoc_docx-to-md.sh
@@ -0,0 +1,10 @@
+# !/usr/bin/env bash
+
+# Convert Microsoft's docx to Markdown for blog posts.
+hello_world="Hello, fellow lovely people :3"
+
+
+input_format=".docx"
+output_format=".md"
+
+echo $hello_world
